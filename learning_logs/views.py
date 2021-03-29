63393c8626b3899ff9a19fd71bcd1b5bde360adf
tetsubinmaru <tesoro5963@gmail.com>
2021-03-30 07:55:47 +0900

    fifth commit

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Topic
+from .forms import TopicForm
 
 # Create your views here.
 
@@ -16,3 +17,18 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
+
+def new_topic(request):
+    if request.method != "POST":
+        # データは送信されていないので、空のフォームを生成する
+        form = TopicForm()
+    else:
+        # POSTでデータが送信されたのでこれを処理する
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learing_logs:topics')
+    
+    # 空または無効のフォームを表示する
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
