2c5888a4c782ece44aa10738cc71912fa4e1ad52
tetsubinmaru <tesoro5963@gmail.com>
2021-03-31 07:44:57 +0900

    seventh commit

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('new_topic/', views.new_topic, name='new_topic'),
     # 新規記事の追加ページ
     path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry'),
+    # 記事の編集ページ
+    path('edit_entry/<int:entry_id>/', views.edit_entry, name="edit_entry"),
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import Topic
+from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
 
 # Create your views here.
@@ -48,3 +48,21 @@ def new_entry(request, topic_id):
     
     context = {'topic': topic, 'form': form}
     return render(request, 'learning_logs/new_entry.html', context)
+
+def edit_entry(request, entry_id):
+    # 既存の記事を編集する
+    entry = Entry.object.get(id=entry_id)
+    topic = entry.topic
+
+    if request != 'POST':
+        # 初回リクエスト時は現在の記事の内容がフォームに埋め込まれている
+        form = EntryForm(instance=entry)
+    else:
+        # POSTでデータが送信されたので、これを処理する
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topic', topic_id=topic.id)
+    
+    context = {'entry': entry, 'topic': topic, 'form': form}
+    return render(request, 'learning_logs/edit_entry.html', context)
