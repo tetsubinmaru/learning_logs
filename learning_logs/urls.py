e4c055688c482354b6ee44b715fc22b65d01b2c8
tetsubinmaru <tesoro5963@gmail.com>
2021-03-30 21:33:41 +0900

    fifth commit

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('topics/', views.topics, name='topics'),
     path('topics/<int:topic_id>/', views.topic, name="topic"),
-    path('new_topic/', views.new_topic, nme='new_topic'),
+    path('new_topic/', views.new_topic, name='new_topic'),
+    # 新規記事の追加ページ
+    path('new_entry/<int:topic_id>/', views.new_entry, name="new_entry"),
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Topic
-from .forms import TopicForm
+from .forms import TopicForm, EntryForm
 
 # Create your views here.
 
@@ -32,3 +32,19 @@ def new_topic(request):
     # 空または無効のフォームを表示する
     context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
+
+def new_entry(request, topic_id):
+    topic = Topic.objects.get(id=topic_id)
+
+    if request.method != "POST":
+        form = EntryForm()
+    else:
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return redirect('learning_logs:topic', topic_id=topic_id)
+    
+    context = {'topic': topic, 'form': form}
+    return render(redirect, 'learning_logs/new_entry.html', context)
