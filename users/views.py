d9a38c26a075fcc8074c31244a01a3880febe380
tetsubinmaru <tesoro5963@gmail.com>
2021-04-01 20:59:58 +0900

    commit

--- users/views.py
+++ users/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+def register(request):
+    """新しいユーザを登録する"""
+    if request.method != "POST":
+        # 空のユーザ登録フォームを表示する
+        form = UserCreationForm()
+    else:
+        # 入力済みのフォームを処理する
+        form = UserCreationForm(data=request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            #ユーザをログインさせてホームページにリダイレクトする
+            login(request, new_user)
+            return redirect('learning_logs:index')
+
+    # 空または無効のフォームを表示する
+    context = {'form': form}
+    return render(request, 'registration/register.html', context)
